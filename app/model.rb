4e9812858a1d9af476e8e6218dcca6a4b57d044e
Robert Ignasiak <tori@robert-i.com>
2014-05-23 03:41:17 +0200

    Another try

--- app/model.rb
+++ app/model.rb
@@ -1,7 +1,12 @@
 require 'pg'
 
 def main
-    conn = PG.connect(ENV['DATABASE_URL'])
+    db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
+    username = db_parts[3]
+    password = db_parts[4]
+    host = db_parts[5]
+    db = db_parts[7]
+    conn = PGconn.open(:host =>  host, :dbname => db, :user=> username, :password=> password)
 end
 
 main
