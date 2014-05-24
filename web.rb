dd1a80dc9e1a38ab05add4db35dd07b3be0fed8c
Robert Ignasiak <tori@robert-i.com>
2014-05-24 02:00:30 +0200

    Added terminal view

--- web.rb
+++ web.rb
@@ -2,9 +2,16 @@ require 'sinatra'
 require './app/model'
 
 get '/' do
-	"Hello word"
+	erb :index
 end
 
 get '/admin' do
 	createModel
+end
+
+post '/ask' do
+	if params[:query] == 'login = torianin'
+		return "Witaj w mega nielegalnym sklepie"
+	else
+		return "Wogóle nie mam pojęcia kim jesteś"
 end
