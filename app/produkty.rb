7df3e2f7a6a3790352966f4fd3ce0eb8cffe53c3
Robert Ignasiak <tori@robert-i.com>
2014-06-05 10:16:05 +0200

    Added start and end tag

--- app/produkty.rb
+++ app/produkty.rb
@@ -2,6 +2,7 @@ require './app/model'
 
 def printProducts
 	p = PostgresConnector.new
+	puts p
 	value = ""
 	p.getProducts{|row| value = value + row['category'] + " " + row['effects'] + " " + row['discription']}
 	value
