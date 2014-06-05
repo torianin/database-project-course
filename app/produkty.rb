524a78a2f3dd8ab42e354fa0d5467e2a80f1b87e
Robert Ignasiak <tori@robert-i.com>
2014-06-05 11:32:43 +0200

    Added start and end tag

--- app/produkty.rb
+++ app/produkty.rb
@@ -28,7 +28,7 @@ def printProducts
 	value = ""
   p.getConnector.exec( "SELECT * FROM products" ) do |result|
     result.each do |row|
-      value = value +"Kategoria : " + category(row['category']) + "Efekt : " + effect(row['effects']) + "Opis : " + row['discription']
+      value = value +"Kategoria : " + category(row['category']) + "\nEfekt : " + effect(row['effects']) + "\nOpis : " + row['discription']
     end
   end
 	value
