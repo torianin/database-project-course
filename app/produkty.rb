993cb93c02218d86d959da1e488f53bd2fa0fdd3
Robert Ignasiak <tori@robert-i.com>
2014-06-05 11:29:12 +0200

    Added start and end tag

--- app/produkty.rb
+++ app/produkty.rb
@@ -8,13 +8,25 @@ def category(category)
 	end
 end
 
+def effect(effect)
+  when "0" then return "Uczucie odprężenia"
+  when "1" then return "Słabsza reakcja na ból"
+  when "2" then return "Euforia"
+  when "3" then return "Senność"
+  when "4" then return "Błogostan"
+  when "5" then return "Brak potrzeby snu"
+  when "6" then return "Widzisz dźwięki i słyszysz kolory"
+  when "7" then return "Podniecenie"
+  when "8" then return "Urojenie i halucynacje wzrokowe"
+end
+
 def printProducts
 	p = PostgresConnector.instance
 	puts p
 	value = ""
   p.getConnector.exec( "SELECT * FROM products" ) do |result|
     result.each do |row|
-      value = value + category(row['category']) + " " + row['effects'] + " " + row['discription']
+      value = value +"Kategoria : " category(row['category']) + "Effekt : " + effect(row['effects']) + "Opis : " + row['discription']
     end
   end
 	value
