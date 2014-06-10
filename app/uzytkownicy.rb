d6b2771190607b2d17eef466de517704f35c9730
Robert Ignasiak <tori@robert-i.com>
2014-06-10 04:05:16 +0200

    Add user

--- app/uzytkownicy.rb
+++ app/uzytkownicy.rb
@@ -6,7 +6,7 @@ def printUsers
 	value = ""
   p.getConnector.exec( "SELECT * FROM users" ) do |result|
     result.each do |row|
-      value = value +"Użytkownicy : " + category(row['id_user']) + ". " + effect(row['login']) + ", " + row['role'] + "\n" + row['data'] + "\n"
+      value = value + row['id_user'] + " : "  + ". " + row['login'] + ", " + row['role'] + "\n" + row['data'] + "\n"
     end
   end
 	value
