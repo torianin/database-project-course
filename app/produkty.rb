dbd3d9e6b2e909b12b20150cb2db5723c6b20af0
Robert Ignasiak <tori@robert-i.com>
2014-06-18 18:28:13 +0200

    Dodanie wyświetlania tabeli

--- app/produkty.rb
+++ app/produkty.rb
@@ -34,13 +34,16 @@ end
 
 def printProducts
 	p = PostgresConnector.instance
-	rows = ['Id','Kategoria','Efekt','Opis']
   value = ""
   p.getConnector.exec( "SELECT * FROM products" ) do |result|
     result.each do |row|
       value = value + "\n" + row['id_product'] + "\t" + category(row['category']) + "\t" + effect(row['effects']) + "\t" + row['discription']
     end
   end
+  rows = []
+  rows << ['One', 1]
+  rows << ['Two', 2]
+  rows << ['Three', 3]
 	table = Terminal::Table.new :rows => rows
   table.to_s
 end
