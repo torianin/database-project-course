ea6ff98b1261ff0c0540ca7cdf26365e4ce9015d
Robert Ignasiak <tori@robert-i.com>
2014-06-18 15:49:45 +0200

    Dodanie słownika

--- app/checker.rb
+++ app/checker.rb
@@ -17,11 +17,7 @@ class Dictionary
 	          suggestions = @dict.suggest(checklist[0])
 	          if (suggestions.size)
 	              @correction = suggestions.first
-	              return_messages = Array.new()
-	              return_messages[0] = "Chyba coś źle napisałeś, nie chodziło ci przypadkiem o słowo #{@correction}."
-	              return_messages[1] = "Wydaje mi się, że miałeś na myśli słowo #{@correction}."
-	              return_messages[2] = "A nie powinno być #{@correction} zamiast #{checklist[0]}."
-	              return return_messages[Random.rand(3)]
+	              return return_messages = "Wydaje mi się, że miałeś na myśli słowo #{@correction}."
 	          end
 	      end
         if !checkvalue
