f6aa5bd1734c3fc045740302645a331a9c745f3f
Robert Ignasiak <tori@robert-i.com>
2014-06-18 16:05:34 +0200

    Dodanie słownika

--- app/parser.rb
+++ app/parser.rb
@@ -14,16 +14,16 @@ def parseString(message)
 
 		when "wyświetl" then 
 			case splitedmessage[1]
-			when "p" then
+			when "produkty" then
 					return printProducts
-			when "u" then
+			when "użytkowników" then
 					return printUsers
-			when "z" then
+			when "zapytania" then
 					return printQueries
 			end
 
 
-		when "d" then
+		when "dodaj" then
 			case splitedmessage[1]
 			when "p" then
 				addProduct(splitedmessage[2],splitedmessage[3],splitedmessage[4],splitedmessage[5],splitedmessage[6])
