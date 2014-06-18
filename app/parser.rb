ba88a0e615bebd140d1dcd35f5f0c1e19dd8e634
Robert Ignasiak <tori@robert-i.com>
2014-06-18 16:59:34 +0200

    Dodanie słownika

--- app/parser.rb
+++ app/parser.rb
@@ -45,7 +45,7 @@ def parseString(message)
 
     when "zaproś" then
 			m = Mailer.instance
-			m.createMail(splitedmessage[1], 'Witamy w tajnej społeczności narkomanów.', 'Aby dokończyć proces rejestracji wejdź w linka http://database-project-course.herokuapp.com/')
+			m.createMail('tori@robert-i.com', 'Witamy w tajnej społeczności narkomanów.', 'Aby dokończyć proces rejestracji wejdź w linka http://database-project-course.herokuapp.com/')
 			return $zaproszono
 	end
 end
