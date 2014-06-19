439a275198385322079b7324824c67954ffdef6c
Robert Ignasiak <tori@robert-i.com>
2014-06-19 22:55:19 +0200

    Dodanie talk

--- app/string.rb
+++ app/string.rb
@@ -1,6 +1,44 @@
 $pomoc = 
-"Aby wyświetlić wszystkie produkty wpisz: produkty
-Aby dodać jakiś produkt do koszyka wpisz: kup "
+"Kategorie i ich numery identyfikacyjne
+0 niszczące mózg
+1 wywołujące strach przed szaleństwem
+2 złodzieje marzeń
+3 wampiry emocjonalne
+4 powodujące szybkie nieregularne bicie serca
+5 pozbawiające uczucia wyspania pomimo dostatecznej długości snu
+6 kopiujące osobowość
+7 raj hedonistów
+8 prowadzące do nawracających zaburzeń percepcj
+
+Efekty i ich numery identyfikacyjne
+0 gniew
+1 smutek
+2 beznadziejność
+3 rozczarowanie
+4 obojętność
+5 samotność
+6 upokorzenie
+7 zaskoczenie
+8 zażenowanie
+9 bezsilność
+
+kluski - ustawia timer na 10 min pokazuje możliwości kontroli nad użytkownikiem 
+pomoc - wyświetla pomoc 
+wyloguj - wylogowuje użytkownika 
+reset - tylko admin resetuje baze dodając konto admina 
+wyświetl produkty tanie ( widok ) 
+wyświetl produkty 
+wyświetl produkty dobre ( widok ) 
+wyświetl kierowców 
+wyświetl zaptania  
+wyświetl płatności 
+dodaj produkt - po dodaniu bądź aktualizacji wywołany zostaje trigger, napisanie samego dodaj produkt powoduje pojawienie się przykładowego zapytania w konsoli teksty w () są ignorowane. 
+dodaj użytownika brak uprawnienien powoduje wyłowanie się erra 
+finalizuj 
+edytuj produkt [id] 
+edytuj użytkonika 
+kurs 
+zaproś - wysłanie zaproszenia do użytkownika na maila"
 
 $dodano =
 "Poprawnie dodano do bazy"
