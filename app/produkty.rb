3a6564cd0a741f64e511b82385f2f43066d922c9
Robert Ignasiak <tori@robert-i.com>
2014-06-19 17:18:11 +0200

    Dodanie obrazków

--- app/produkty.rb
+++ app/produkty.rb
@@ -62,7 +62,7 @@ def addProduct(category, effects, discription, prise, current_tax)
   p.getConnector.exec_prepared("insert_products", [category, effects, discription, prise, current_tax])
 end
 
-def editProduct(id, ategory, effects, discription, prise, current_tax)
+def editProduct(id, category, effects, discription, prise, current_tax)
   p = PostgresConnector.instance
   p.getConnector.exec_prepared("update_products", [category, effects, discription, prise, current_tax, id])
 end
